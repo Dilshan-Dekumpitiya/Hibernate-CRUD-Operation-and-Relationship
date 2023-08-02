99dd4b893824fc657ca717e9f7c3c6616c5902f2
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-08-02 16:29:14 +0530

    add loadAllBooks method to crud operation module using Hibernate query language(HQL)

--- 1.CRUD-operations-on-a-single-entity/src/main/java/org/assignment/Main.java
+++ 1.CRUD-operations-on-a-single-entity/src/main/java/org/assignment/Main.java
@@ -1,25 +1,71 @@
 package org.assignment;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
 import org.assignment.entity.Book;
 import org.assignment.util.FactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Book book=new Book("B001","Madol Duuwa","M Wickramasinghe");
+
+       //   saveBook();
+        //  loadAllBooks();
+        loadAllBooksWithCriteria();
+
+    }
+
+    //without HQL to load all books (Using CriteriaBuilder)
+    private static void loadAllBooksWithCriteria() {
+        Session session= FactoryConfiguration.getInstance().getSession();
+
+        //create the criteria
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<Book> query = criteriaBuilder.createQuery(Book.class);
+        query.from(Book.class);
+        List<Book> resultList = session.createQuery(query).getResultList();
+        for (Book book:
+             resultList) {
+            System.out.println(book);
+        }
+    }
+
+    //Using HQL
+    private static void loadAllBooks() {
+        Session session= FactoryConfiguration.getInstance().getSession();
+        Query query = session.createQuery("FROM Book ");//HQL --> Hibernate Query Language
+        List<Book> bookList= query.list();
+        for (Book book:bookList) {
+            System.out.println(book);
+        }
+
+    }
+
+    //session.remove(book); //remove data from table
+    // session.update(book);//update data from table
+
+    private static void saveBook() {
+
+        Book book=new Book("B002","Hath Pana","M Wickramasinghe");
 
         //start session
         Session session= FactoryConfiguration.getInstance().getSession(); //Session is Interface (Therefore can't initialize with new keyword)
 
         Transaction transaction=session.beginTransaction(); //begin transaction
-       // session.persist(book); //add data to table
-        //session.remove(book); //remove data from table
-         session.update(book);//update data from table
+        session.persist(book); //add data to table
 
         transaction.commit(); //commit that transaction
 
         session.close(); //close the session
+
     }
 
 }
