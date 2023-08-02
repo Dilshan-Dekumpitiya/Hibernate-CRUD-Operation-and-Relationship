c2c78ab8b8825284ac1f5e2e87455959db077482
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-08-02 16:29:14 +0530

    add loadAllBooks method to crud operation module using Hibernate query language(HQL)

--- 1.CRUD-operations-on-a-single-entity/src/main/java/org/assignment/Main.java
+++ 1.CRUD-operations-on-a-single-entity/src/main/java/org/assignment/Main.java
@@ -4,22 +4,47 @@ import org.assignment.entity.Book;
 import org.assignment.util.FactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Book book=new Book("B001","Madol Duuwa","M Wickramasinghe");
+
+       //   saveBook();
+          loadAllBooks();
+
+    }
+
+    private static void loadAllBooks() {
+        Session session= FactoryConfiguration.getInstance().getSession();
+        Query query = session.createQuery("FROM Book ");//HQL --> Hibernate Query Language
+        List<Book> bookList= query.list();
+        for (Book book:bookList) {
+            System.out.println(book);
+        }
+
+    }
+
+    //session.remove(book); //remove data from table
+    // session.update(book);//update data from table
+
+    private static void saveBook() {
+
+        Book book=new Book("B002","Hath Pana","M Wickramasinghe");
 
         //start session
         Session session= FactoryConfiguration.getInstance().getSession(); //Session is Interface (Therefore can't initialize with new keyword)
 
         Transaction transaction=session.beginTransaction(); //begin transaction
-       // session.persist(book); //add data to table
-        //session.remove(book); //remove data from table
-         session.update(book);//update data from table
+        session.persist(book); //add data to table
 
         transaction.commit(); //commit that transaction
 
         session.close(); //close the session
+
     }
 
 }
